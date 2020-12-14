6411b64f7385a7644f12ae31d2d7c618a827a530
Misha Khmara <1927mishakhmara03@gmail.com>
2020-12-14 16:57:20 +0200

    Исправил ошибки

--- js/task-4.js
+++ js/task-4.js
@@ -1,22 +1,26 @@
 let credits = 23580;
 const pricePerDroid = 3000;
 let totalPrice;
-let userInput = prompt (' Сколько дроидов Вы хотите купить?');
+let userInput = prompt(' Сколько дроидов Вы хотите купить?');
 userInput = Number(userInput);
-console.log(userInput)
+console.log(userInput);
 
-if(userInput === 'nall'){
-    console.log('Отменено пользователем!');
-    alert ('Отменено пользователем!');
-} else{
-    totalPrice = userInput*pricePerDroid;
-    console.log(totalPrice);
-    if (totalPrice > credits){
-    console.log('Недостаточно средств на счету!')
-    alert('Недостаточно средств на счету!')
-    } else {
-        credits -= totalPrice;
-        console.log(`Вы купили ${userInput} дроидов, на счету осталось ${credits} кредитов.`);
-        alert(`Вы купили ${userInput} дроидов, на счету осталось ${credits} кредитов.`);
-}
+if (userInput === 'null') {
+  console.log('Отменено пользователем!');
+  alert('Отменено пользователем!');
+} else {
+  totalPrice = userInput * pricePerDroid;
+  console.log(totalPrice);
+  if (totalPrice > credits) {
+    console.log('Недостаточно средств на счету!');
+    alert('Недостаточно средств на счету!');
+  } else {
+    credits -= totalPrice;
+    console.log(
+      `Вы купили ${userInput} дроидов, на счету осталось ${credits} кредитов.`,
+    );
+    alert(
+      `Вы купили ${userInput} дроидов, на счету осталось ${credits} кредитов.`,
+    );
+  }
 }
--- js/task-5.js
+++ js/task-5.js
@@ -1,43 +1,36 @@
 let country = prompt('Укажите страну в которую хотите совершить доставку');
 console.log(country);
-const normalizedInput = country.toLowerCase()
-console.log(normalizedInput)
-let price ;
+const normalizedInput = country.toLowerCase(0);
+console.log(normalizedInput);
+let price;
 
- 
 switch (country) {
-    case 'Китай': {
-        price = 100;
-        console.log(`Доставка в ${country}, будет стоить ${price} кредитов`);
-          alert(`Доставка в ${country}, будет стоить ${price} кредитов`);
-        break;
-    }
-          case 'Чили': {
-        price = 250;
-        console.log(`Доставка в ${country}, будет стоить ${price} кредитов`);
-        alert(`Доставка в ${country}, будет стоить ${price} кредитов`);
-        break;
-    }
-          case 'Австралия': {
-        price = 170;
-        console.log(`Доставка в ${country}, будет стоить ${price} кредитов`);
-         alert(`Доставка в ${country}, будет стоить ${price} кредитов`);
-        break;
-    }
-          case 'Индия': {
-        price = 80;
-        console.log(`Доставка в ${country}, будет стоить ${price} кредитов`);
-         alert(`Доставка в ${country}, будет стоить ${price} кредитов`);
-        break;
-    }
-          case 'Ямайка': {
-        price = 120;
-        console.log(`Доставка в ${country}, будет стоить ${price} кредитов`);
-         alert(`Доставка в ${country}, будет стоить ${price} кредитов`);
-        break;
-    }
-    default:
-        console.log('В вашей стране доставка не доступна');
-        alert('В вашей стране доставка не доступна')
-        
+  case 'Китай': {
+    price = 100;
+
+    break;
+  }
+  case 'Чили': {
+    price = 250;
+
+    break;
+  }
+  case 'Австралия': {
+    price = 170;
+
+    break;
+  }
+  case 'Индия': {
+    price = 80;
+
+    break;
+  }
+  case 'Ямайка': {
+    price = 120;
+
+    break;
+  }
+  default:
+    alert('В вашей стране доставка не доступна');
 }
+alert(`Доставка в ${country}, будет стоить ${price} кредитов`);
