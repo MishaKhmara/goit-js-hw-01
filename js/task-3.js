fb263ddb77c2db103fd5afe58a60096d22d844dd
MishaKhmara <72788711+MishaKhmara@users.noreply.github.com>
2021-01-22 00:03:30 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,9 @@
 const ADMIN_PASSWORD = 'jqueryismyjam';
-let message;
-message = prompt('Введите пороль');
-if (message === null) {
-  alert('Отменено пользователем!');
-} else if (message === ADMIN_PASSWORD) {
+let message = prompt('Введите пароль');
+
+if( message === null){
+  alert('Отменено пользователем!')
+}else if(message === ADMIN_PASSWORD){
   alert('Добро пожаловать!');
-} else if (message !== ADMIN_PASSWORD) {
-  alert('Доступ запрещен, неверный пароль!');
-}
+}else {alert('Доступ запрещен, неверный пароль!');}
+
