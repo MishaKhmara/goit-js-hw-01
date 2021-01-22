e6d9bf2d96e9d5b3a8a2547b53f493fe4cc9312d
MishaKhmara <72788711+MishaKhmara@users.noreply.github.com>
2021-01-22 11:54:10 +0200

    исправил код

--- js/task-4.js
+++ js/task-4.js
@@ -3,22 +3,19 @@ const pricePerDroid = 3000;
 let totalPrice;
 let userInput = prompt(' Сколько дроидов Вы хотите купить?');
 userInput = Number(userInput);
-console.log(userInput);
 
-if (userInput === 'null') {
-  console.log('Отменено пользователем!');
+
+if (!+userInput) {
   alert('Отменено пользователем!');
 } else {
   totalPrice = userInput * pricePerDroid;
-  console.log(totalPrice);
+ 
   if (totalPrice > credits) {
-    console.log('Недостаточно средств на счету!');
+   
     alert('Недостаточно средств на счету!');
   } else {
     credits -= totalPrice;
-    console.log(
-      `Вы купили ${userInput} дроидов, на счету осталось ${credits} кредитов.`,
-    );
+  
     alert(
       `Вы купили ${userInput} дроидов, на счету осталось ${credits} кредитов.`,
     );
